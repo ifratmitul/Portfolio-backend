480455a34b646835954efdfa21b8afa3fa6f87f0
ifratmitul <ifrat.ikhtear@gmail.com>
2022-03-26 00:12:06 +0600

    fixed a bug in certificate controller

--- API/Controllers/CertificateController.cs
+++ API/Controllers/CertificateController.cs
@@ -12,13 +12,13 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddCertificate(Certificate certificate)
+        public async Task<IActionResult> AddCertificate([FromForm] Certificate certificate)
         {
             return HandleResult(await Mediator.Send(new Create.Command { Certificate = certificate }));
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> EditCertificate(Guid id, Certificate certificate)
+        public async Task<IActionResult> EditCertificate(Guid id, [FromForm] Certificate certificate)
         {
             certificate.Id = id;
             return HandleResult(await Mediator.Send(new Edit.Command { Certificate = certificate }));
