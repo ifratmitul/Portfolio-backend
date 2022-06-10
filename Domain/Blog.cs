75b7fe7d722bf85d90ec15e5e798e35e3a53ae4d
ifratmitul <ifrat.ikhtear@gmail.com>
2022-06-10 15:28:18 +0600

    personal blog feature intial commit

---
+++ Domain/Blog.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain;
+public class Blog
+{
+    public Guid Id { get; set; }
+    public string Blog_title { get; set; }
+    public string Blog_article { get; set; }
+    public Photo BlogCoverPhoto { get; set; }
+    public DateOnly DateOfPublish  { get; set; }
+
+
+}
+
